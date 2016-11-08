c5e94e3b7b81bfb8f51fbc4a39617f058840d06c
Antony Jones <aj@desirableobjects.co.uk>
2016-11-08 21:22:31 +0000

    Pass event emitter to widgets

--- test/spec/widget.spec.js
+++ test/spec/widget.spec.js
@@ -1,9 +1,8 @@
 const Widget = require(fromSrc('modules/widget'))
-const WidgetBuilder = require(fromTest('util/widget.builder'))
 const sinon = require('sinon')
 
 describe('modules.widget', () => {
-  const dashboard = { layout: { rows: 4, columns: 5 } }
+  const dashboard = { layout: { rows: 4, columns: 5 }, emitter: { emit: sinon.stub() } }
   const position = {x: 0, y: 0, w: 1, h: 1}
 
   it('Barf on unknown widget', (done) => {
@@ -168,7 +167,7 @@ describe('modules.widget', () => {
     done()
   })
 
-  context.only('Event Emitter', () => {
+  context('Event Emitter', () => {
     let expectedEmitFn
 
     class MyWidget {
@@ -178,16 +177,9 @@ describe('modules.widget', () => {
       }
     }
 
-    const emittingDashboard = { emitter: { emit: function () {} } }
-
-    before((done) => {
-      new Widget(emittingDashboard, position, MyWidget)
-      done()
-    })
-
     it('Recieves an event emitter on construction', (done) => {
+      new Widget(dashboard, position, MyWidget)
       expect(expectedEmitFn).to.be.a.function()
-      console.log(expectedEmitFn)
       done()
     })
   })
