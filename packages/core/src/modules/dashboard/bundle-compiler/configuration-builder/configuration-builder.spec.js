688fe3586130f7f2b322c9d289da0e03de33b773
Antony Jones <aj@desirableobjects.co.uk>
2017-05-06 20:12:26 +0100

    add test for configuration builder

---
+++ packages/core/src/modules/dashboard/bundle-compiler/configuration-builder/configuration-builder.spec.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const builder = require('.')
+const { reach } = require('hoek')
+
+describe('bundle-compiler.configuration-builder', () => {
+  context('Dynamic Contents', () => {
+    it('Contents correctly set', done => {
+      const expected = 'xyz'
+      const config = builder.build(expected)
+      const actual = reach(config, 'entry.contents')
+      expect(actual).to.exist().and.to.equal(expected)
+      done()
+    })
+  })
+})
