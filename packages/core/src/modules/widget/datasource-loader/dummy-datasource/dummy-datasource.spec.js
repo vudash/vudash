8f569cd4ce603ba4a8d392dd1490f037240c1f2f
Antony Jones <aj@desirableobjects.co.uk>
2017-05-06 20:36:00 +0100

    add tests for dummy datasource

---
+++ packages/core/src/modules/widget/datasource-loader/dummy-datasource/dummy-datasource.spec.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const DummyDatasource = require('.')
+
+describe('dummy-datasource', () => {
+  it('can be constructed', done => {
+    expect(DummyDatasource).to.be.a.function()
+    done()
+  })
+
+  it('Does not fetch', done => {
+    const widgetName = 'abczys'
+    const ds = new DummyDatasource({ widgetName })
+    expect(ds.fetch.bind(ds)).to.throw(Error, `Widget ${widgetName} requested data, but no datasource was configured. Check the widget configuration in your dashboard config.`)
+    done()
+  })
+})
