1fe82f235214e38a2ff89577b099185e8776fecd
Antony Jones <aj@desirableobjects.co.uk>
2017-07-04 07:55:59 +0100

    improve cli

--- packages/core/bin/vudash.js
+++ packages/core/bin/vudash.js
@@ -1,27 +1,68 @@
 #!/usr/bin/env node
 
-const Hoek = require('hoek')
+const { textSync } = require('figlet')
+const { yellow, green, blue, bold } = require('chalk')
+const npm = require('npm-programmatic')
+const ora = require('ora')
 const Path = require('path')
 const fs = require('fs-extra')
-const exec = require('child_process').exec
 const dashboard = require('../dashboards/template.json')
-const __cwd = process.cwd()
+const cwd = process.cwd()
+const { version } = require('../package.json')
 
 const [ , , arg ] = process.argv
+
+console.log(
+  yellow(
+    textSync('vudash', {
+      font: 'Slant'
+    })
+  ),
+  blue(
+    `v${version}`
+  )
+)
 
 if (!arg) {
   require('../app')
 }
 
 if (arg === 'create') {
-  console.log('Installing dependencies. This could take a minute or two...')
-  const configFile = Path.join(__cwd, 'dashboards', 'default.json')
-  const packageJson = Path.join(__cwd, 'package.json')
-  fs.ensureDirSync(Path.join(__cwd, 'dashboards'))
-  fs.writeJsonSync(configFile, dashboard)
+  const spinner = ora().start('Creating dashboard layout')
+  const configFile = Path.join(cwd, 'dashboards', 'default.json')
+  const packageJson = Path.join(cwd, 'package.json')
+  const dashboardsDir = Path.join(cwd, 'dashboards')
+
+  fs.ensureDirSync(dashboardsDir)
   fs.writeJsonSync(packageJson, { name: 'my-vudash-dashboard', main: 'vudash', scripts: { start: 'vudash' } })
-  exec('npm install --save moment vudash vudash-widget-time', (error, stdout, stderr) => {
-    Hoek.assert(!error, error)
-    console.log('Created sample dashboard. Run "vudash" or "npm start" to view')
+  fs.writeJsonSync(configFile, dashboard)
+  spinner.succeed('Created dashboard layout')
+  spinner.start('Installing dependencies. This could take a minute or two...')
+
+  npm.install([
+    'vudash',
+    'vudash-widget-time'
+  ], {
+    cwd,
+    save: true
   })
+  .then(() => {
+    spinner.succeed('Installed dependencies.')
+    console.log(
+      green(
+        'Created sample dashboard. Run "vudash" or "npm start" to view'
+      )
+    )
+  })
+  .catch(e => {
+    spinner.fail('Failed to install some dependencies.')
+    console.log(e)
+  })
+}
+
+if (['help', '--help'].includes(arg)) {
+  console.log('Usage: vudash [action]')
+  console.log('with no action, runs the dashboard configured in the current working directory.')
+  console.log('\nactions:')
+  console.log('\n', bold('create'), 'Create a new dashboard')
 }
