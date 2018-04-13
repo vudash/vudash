0444421b8cf0931bfb78677001b484e1a40e96ac
Antony Jones <aj@desirableobjects.co.uk>
2018-04-13 10:12:27 +0100

    allow dashboards to be dynamically added

---
+++ packages/core/src/modules/dashboard/loader/loader.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const { NotFoundError } = require('../../../errors')
+const Dashboard = require('..')
+const fs = require('fs')
+const { join } = require('path')
+
+function load (cache, name, io) {
+  const path = join(process.cwd(), 'dashboards', `${name}.json`)
+
+  if (!fs.existsSync(path)) {
+    throw new NotFoundError(`Dashboard ${name} does not exist.`)
+  }
+
+  const descriptor = require(path)
+  return add(cache, name, io, descriptor)
+}
+
+function add (cache, name, io, descriptor) {
+  const dashboard = Dashboard.create(descriptor, io)
+  dashboard.loadDatasources()
+  dashboard.loadWidgets()
+
+  cache[name] = dashboard
+  return cache[name]
+}
+
+function find (cache, name, io) {
+  return cache[name] || load(cache, name, io)
+}
+
+function has (cache, name) {
+  return !!cache[name]
+}
+
+module.exports = {
+  find,
+  has,
+  add,
+  load
+}
--- packages/core/src/modules/dashboard/loader/loader.spec.js
+++ packages/core/src/modules/dashboard/loader/loader.spec.js
@@ -6,6 +6,37 @@ const { expect } = require('code')
 
 describe('dashboard/loader', () => {
   it('Dashboard is not found', () => {
-    expect(() => { return loader.load({}, 'xyz') }).to.throw(NotFoundError, 'Dashboard xyz does not exist.')
+    expect(() => {
+      return loader.load({}, 'xyz')
+    }).to.throw(NotFoundError, 'Dashboard xyz does not exist.')
+  })
+
+  context('#add()', () => {
+    const cache = {}
+    const emitter = { on: () => { } }
+    const descriptor = { layout: { columns: 0, rows: 0 }, widgets: [] }
+
+    beforeEach(() => {
+      loader.add(cache, 'xyz', emitter, descriptor)
+    })
+
+    it('Add dashboard to cache', () => {
+      expect(cache.xyz.descriptor).to.equal(descriptor)
+    })
+
+    it('Find dashboard from cache', () => {
+      expect(loader.find(cache, 'xyz', emitter).descriptor).to.equal(descriptor)
+    })
+  })
+
+  context('#has()', () => {
+    const cache = { 'abc': {} }
+    it('is contained in cache', () => {
+      expect(loader.has(cache, 'abc')).to.be.true()
+    })
+
+    it('is not contained in cache', () => {
+      expect(loader.has(cache, 'xyz')).to.be.false()
+    })
   })
 })
