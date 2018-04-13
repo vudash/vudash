c7c4fdb351c71866fa0505f264305a5c721361a5
Antony Jones <aj@desirableobjects.co.uk>
2018-04-13 17:21:08 +0100

    update tests

--- packages/core/src/modules/widget/validator/validator.js
+++ packages/core/src/modules/widget/validator/validator.js
@@ -1,8 +1,15 @@
 'use strict'
 
 const configValidator = require('../../config-validator')
+const Joi = require('joi')
 
-exports.validate = function (name, schema, options) {
-  if (!schema) { return options }
+const defaultSchema = {
+  description: Joi.string().optional().description('Widget display label')
+}
+
+exports.validate = function (name, baseSchema, options) {
+  if (!baseSchema) { return options }
+
+  const schema = baseSchema.keys(defaultSchema)
   return configValidator.validate(name, schema, options)
 }
