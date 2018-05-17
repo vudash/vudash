70eea8e13e617a376040bf1865139ccaf69b7684
Antony Jones <aj@desirableobjects.co.uk>
2018-05-17 10:18:28 +0100

    fix tests for widget-statistic

--- packages/widget-statistic/src/server/widget.spec.js
+++ packages/widget-statistic/src/server/widget.spec.js
@@ -5,36 +5,38 @@ const { register, validation } = require('./widget')
 const Joi = require('joi')
 
 describe('widget', () => {
-  it('Uses config', () => {
-    const config = { foo: 'bar' }
-    const configuration = register(config)
-    expect(configuration.config.foo).to.equal(config.foo)
+  context('#register()', () => {
+    it('Uses config', () => {
+      const config = { foo: 'bar' }
+      const widget = register(config, validation)
+      expect(widget.config.foo).to.equal(config.foo)
+    })
+
+    it('Has default config values', () => {
+      const config = {}
+      const widget = register(config)
+      expect(widget.config.description).to.equal('Statistics')
+    })
+
+    it('Merges config with default values', () => {
+      const config = { description: 'hello' }
+      const widget = register(config)
+      expect(widget.config.description).to.equal(config.description)
+    })
   })
 
-  it('Has default config values', () => {
-    const config = {}
-    const configuration = register(config)
-    expect(configuration.config.description).to.equal('Statistics')
-  })
-
-  it('Merges config with default values', () => {
-    const config = { description: 'hello' }
-    const configuration = register(config)
-    expect(configuration.config.description).to.equal(config.description)
-  })
-
-  it('Will convert given value to string', () => {
-    const configuration = register({ format: '%s' })
-    const { displayValue } = configuration.update(2)
-    expect(displayValue).to.equal('2')
-  })
-
-  context('Formatting', () => {
+  context('Updates', () => {
     let output
 
     beforeEach(() => {
       const configuration = register({ format: '%d%%' })
       output = configuration.update(34)
+    })
+
+    it('Will convert given value to string', () => {
+      const widget = register({ format: '%s' })
+      const { displayValue } = widget.update(2)
+      expect(displayValue).to.equal('2')
     })
 
     it('Will format according to format config', () => {
@@ -46,23 +48,21 @@ describe('widget', () => {
     })
   })
 
-  context('Colours', () => {
+  context('Configuration', () => {
     it('With provided colour', () => {
-      const conf = { colour: '#fff' }
-      const { config } = register(conf)
-      expect(config.colour).to.equal('#fff')
+      const conf = { colour: '#f00' }
+      const config = Joi.attempt(conf, validation)
+      expect(config.colour).to.equal('#f00')
     })
 
     it('No colour passed', () => {
-      const { config } = register({})
-      expect(config.colour).not.to.exist()
+      const config = Joi.attempt({}, validation)
+      expect(config.colour).to.equal('#fff')
     })
-  })
 
-  context('Configuration', () => {
     it('default format is set', () => {
-      const { value } = Joi.validate({}, validation)
-      expect(value.format).to.equal('%s')
+      const config = Joi.attempt({}, validation)
+      expect(config.format).to.equal('%s')
     })
   })
 })
