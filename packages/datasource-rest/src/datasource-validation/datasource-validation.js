ace010b53e12720ff310a1148b9be8e061a04657
Antony Jones <aj@desirableobjects.co.uk>
2018-10-02 17:23:49 +0100

    fix rest datasource validation

--- packages/datasource-rest/src/datasource-validation/datasource-validation.js
+++ packages/datasource-rest/src/datasource-validation/datasource-validation.js
@@ -6,6 +6,6 @@ exports.validation = {
   url: Joi.string().description('Url to call'),
   method: Joi.string().optional().default('get').only('get', 'post', 'put', 'options', 'delete', 'head').description('Http Method'),
   headers: Joi.object().optional().description('additional headers'),
-  payload: Joi.object().optional().description('request payload'),
+  body: Joi.object().optional().description('request body'),
   graph: Joi.string().optional().description('Graph expression (json path) to reach json values')
 }
